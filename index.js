7c8b3b9ed71293d34ef42d7710a472d89f1e2cb1
Stuart <stu.r.williams@gmail.com>
2015-11-03 20:21:05 +0000

    Updated comments

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 var diacritics = require('./lib/diacritics'),
     formating = require('./lib/formating');
 
+/**
+ * String Utils Module
+ * @module string-utils
+ */
 module.exports = {
+
+    /**
+     * Removes all non alphanumeric chars allows whitespace
+     * @param {string} str - this string
+     * @returns {string}
+     */
     alphanumeric: diacritics.alphanumeric,
+
+    /**
+     * Replace all diacritics with their basic latin equivalent, unknown equivalents are removed
+     * @param {string} str - the string to purify
+     * @returns {string}
+     */
     purify: diacritics.purify,
+
+    /**
+     * Replaces all spaces in a string with a hyphen
+     * @param {string} str - this string
+     * @returns {string}
+     */
     hyphenate: formating.hyphenate,
+
+    /**
+     * Replaces all spaces in a string with a underscores
+     * @param {string} str - this string
+     * @returns {string}
+     */
     underscore: formating.underscore,
+
+    /**
+     * Purifies, strips non alphanumeric chars, hyphenates spaces and converts to lowercase
+     * @param {string} str - this string
+     * @returns {string}
+     */
     websafe: formating.websafe,
+
+    /**
+     * Purifies, strips non alphanumeric chars, replaces space with underscore and converts to uppercase
+     * @param str {string} - this string
+     * @returns {string}
+     */
     fwssid: formating.fwssid
 };
